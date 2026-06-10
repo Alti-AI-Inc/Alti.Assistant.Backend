f7601ad4acc08af29407534125cf91b8f44a7283
Michael Meram <meram.michael@gmail.com>
2026-06-09 21:53:15 -0400

    docs: autonomous documentation for forum.model.js

--- src/app/modules/forum/forum.model.js
+++ src/app/modules/forum/forum.model.js
@@ -1,7 +1,30 @@
+/**
+ * @file Defines the Mongoose schema and model for Forum posts.
+ * @module app/modules/forum/forum.model
+ */
+
 const mongoose = require('mongoose');
 const validator = require('validator');
 const { categoryValues } = require('./forum.constant');
 
+/**
+ * Mongoose schema for a Forum post.
+ *
+ * @typedef {Object} ForumSchema
+ * @property {string} title - The title of the forum post. Must be between 3 and 100 characters.
+ * @property {string} img - The URL or path to the forum's main image.
+ * @property {string} category - The category of the forum post. Must be one of the values defined in `categoryValues`.
+ * @property {mongoose.Schema.Types.ObjectId} author - The ID of the user who authored the forum post. References the 'User' model.
+ * @property {mongoose.Schema.Types.ObjectId[]} userActivities - An array of IDs referencing user activities related to this forum post. References the 'forum-User-Activities' model.
+ * @property {string} authorEmail - The email address of the author, validated for format and stored in lowercase.
+ * @property {Array<Object>} description - An array of sub-documents, each representing a part of the forum's description.
+ * @property {string} [description[].title] - Optional title for this description part.
+ * @property {string} [description[].content1] - First content paragraph/section for this description part.
+ * @property {string} [description[].content2] - Second content paragraph/section for this description part.
+ * @property {mongoose.Schema.Types.ObjectId} [tenantId=null] - The ID of the tenant this forum post belongs to, for multi-tenancy support. References the 'Tenant' model.
+ * @property {Date} createdAt - The timestamp when the forum post was created. Automatically managed by Mongoose.
+ * @property {Date} updatedAt - The timestamp when the forum post was last updated. Automatically managed by Mongoose.
+ */
 const forumSchema = mongoose.Schema(
   {
     title: {
@@ -69,6 +92,15 @@ const forumSchema = mongoose.Schema(
   }
 );
 
+/**
+ * Represents the Mongoose model for a Forum post.
+ *
+ * @type {mongoose.Model<ForumSchema>}
+ */
 const Forum = mongoose.model('Forum', forumSchema);
 
+/**
+ * Exports the Forum Mongoose model.
+ * @type {mongoose.Model<ForumSchema>}
+ */
 module.exports = Forum;
